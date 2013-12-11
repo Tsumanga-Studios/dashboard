afdca6feb261285ccd872136144506892f4b4481
Peter <peter@tsumanga.com>
2013-12-11 14:58:04 +0000

    Dummy page to test chart drawing

--- dashboard/pages.py
+++ dashboard/pages.py
@@ -10,6 +10,12 @@ class VersionPage(web.RequestHandler):
     def get(self):
         self.write(dashboard.version)
 
+class DummyPage(web.RequestHandler):
+    def get(self, chartType):
+        self.render("chart.html", chartType=chartType)
 
 def urls():
-    return [(r"/dashboard/version", VersionPage)]
+    return [
+        (r"/dashboard/version", VersionPage),
+        (r"/dashboard/dummy/([a-zA-Z]+)", DummyPage),
+        ]
